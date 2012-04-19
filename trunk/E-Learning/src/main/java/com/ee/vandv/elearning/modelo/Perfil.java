e700da91ed68aaa9e9994e93e9cedd4d495fe4a8
stevenziggiz@gmail.com <stevenziggiz@gmail.com@92287979-d72c-0f7f-4cdb-9056247f9e4d>
2012-04-19 06:48:36 +0000

    entities

---
+++ trunk/E-Learning/src/main/java/com/ee/vandv/elearning/modelo/Perfil.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ee.vandv.elearning.modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author stevenziggiz
+ */
+@Entity
+@Table(name = "perfil", catalog = "elearning", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Perfil.findAll", query = "SELECT p FROM Perfil p"),
+    @NamedQuery(name = "Perfil.findByIdperfil", query = "SELECT p FROM Perfil p WHERE p.idperfil = :idperfil"),
+    @NamedQuery(name = "Perfil.findByNombre", query = "SELECT p FROM Perfil p WHERE p.nombre = :nombre")})
+public class Perfil implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idperfil", nullable = false)
+    private Integer idperfil;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "nombre", nullable = false, length = 45)
+    private String nombre;
+    @OneToMany(mappedBy = "idperfil", fetch = FetchType.LAZY)
+    private List<Usuario> usuarioList;
+
+    public Perfil() {
+    }
+
+    public Perfil(Integer idperfil) {
+        this.idperfil = idperfil;
+    }
+
+    public Perfil(Integer idperfil, String nombre) {
+        this.idperfil = idperfil;
+        this.nombre = nombre;
+    }
+
+    public Integer getIdperfil() {
+        return idperfil;
+    }
+
+    public void setIdperfil(Integer idperfil) {
+        this.idperfil = idperfil;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @XmlTransient
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
+    }
+
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idperfil != null ? idperfil.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Perfil)) {
+            return false;
+        }
+        Perfil other = (Perfil) object;
+        if ((this.idperfil == null && other.idperfil != null) || (this.idperfil != null && !this.idperfil.equals(other.idperfil))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.ee.vandv.elearning.modelo.Perfil[ idperfil=" + idperfil + " ]";
+    }
+    
+}
---
+++ trunk/E-Learning/src/main/java/com/ee/vandv/elearning/modelo/Persona.java
@@ -0,0 +1,120 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ee.vandv.elearning.modelo;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author stevenziggiz
+ */
+@Entity
+@Table(name = "persona", catalog = "elearning", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Persona.findAll", query = "SELECT p FROM Persona p"),
+    @NamedQuery(name = "Persona.findByIdpersona", query = "SELECT p FROM Persona p WHERE p.idpersona = :idpersona"),
+    @NamedQuery(name = "Persona.findByNombre", query = "SELECT p FROM Persona p WHERE p.nombre = :nombre"),
+    @NamedQuery(name = "Persona.findByApellido1", query = "SELECT p FROM Persona p WHERE p.apellido1 = :apellido1"),
+    @NamedQuery(name = "Persona.findByApellido2", query = "SELECT p FROM Persona p WHERE p.apellido2 = :apellido2")})
+public class Persona implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "idpersona", nullable = false)
+    private Integer idpersona;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "nombre", nullable = false, length = 45)
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "apellido1", nullable = false, length = 45)
+    private String apellido1;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "apellido2", nullable = false, length = 45)
+    private String apellido2;
+
+    public Persona() {
+    }
+
+    public Persona(Integer idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public Persona(Integer idpersona, String nombre, String apellido1, String apellido2) {
+        this.idpersona = idpersona;
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+    }
+
+    public Integer getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(Integer idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idpersona != null ? idpersona.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Persona)) {
+            return false;
+        }
+        Persona other = (Persona) object;
+        if ((this.idpersona == null && other.idpersona != null) || (this.idpersona != null && !this.idpersona.equals(other.idpersona))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.ee.vandv.elearning.modelo.Persona[ idpersona=" + idpersona + " ]";
+    }
+    
+}
